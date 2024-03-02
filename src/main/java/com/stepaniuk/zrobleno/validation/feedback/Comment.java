4e3120d96e668bd67c34b96b69256bfcefe616af
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2024-03-02 12:01:37 +0200

    feat(validation): Added Comment annotation

---
+++ src/main/java/com/stepaniuk/zrobleno/validation/feedback/Comment.java
@@ -0,0 +1,18 @@
+package com.stepaniuk.zrobleno.validation.feedback;
+
+import jakarta.validation.Constraint;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.hibernate.validator.constraints.Length;
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(validatedBy = {})
+@Length(min = 1, max = 255)
+public @interface Comment {
+
+}
