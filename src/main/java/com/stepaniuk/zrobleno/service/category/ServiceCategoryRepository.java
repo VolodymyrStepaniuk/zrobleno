53caba7f35a569aa65da8aab6587af0a7555957b
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2024-03-02 12:08:01 +0200

    feat(service-category): Added @Repository annotation to ServiceCategoryRepository

--- src/main/java/com/stepaniuk/zrobleno/service/category/ServiceCategoryRepository.java
+++ src/main/java/com/stepaniuk/zrobleno/service/category/ServiceCategoryRepository.java
@@ -2,7 +2,9 @@ package com.stepaniuk.zrobleno.service.category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ServiceCategoryRepository extends JpaRepository<ServiceCategory, Long>,
     JpaSpecificationExecutor<ServiceCategory> {
 
