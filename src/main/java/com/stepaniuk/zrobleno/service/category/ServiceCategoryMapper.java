97ef0fe944134d2be442498ffa5481558cd71f40
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2024-03-02 12:07:37 +0200

    feat(service-category): Added functionality to ServiceCategoryMapper

--- src/main/java/com/stepaniuk/zrobleno/service/category/ServiceCategoryMapper.java
+++ src/main/java/com/stepaniuk/zrobleno/service/category/ServiceCategoryMapper.java
@@ -1,5 +1,24 @@
 package com.stepaniuk.zrobleno.service.category;
 
-public interface ServiceCategoryMapper {
+import com.stepaniuk.zrobleno.service.payload.category.ServiceCategoryResponse;
+import org.mapstruct.AfterMapping;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
+import org.springframework.hateoas.Link;
 
+@Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+public interface ServiceCategoryMapper {
+  @BeanMapping(qualifiedByName = "addLinks")
+  ServiceCategoryResponse toResponse(ServiceCategory serviceCategory);
+
+  @AfterMapping
+  @Named("addLinks")
+  default ServiceCategoryResponse addLinks(ServiceCategory serviceCategory,
+      @MappingTarget ServiceCategoryResponse response) {
+    response.add(Link.of("/services/category/" + serviceCategory.getId()).withSelfRel());
+    return response;
+  }
 }
