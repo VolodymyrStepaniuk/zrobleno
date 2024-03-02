91a96bfc3a259d7a773e43bd657ffce61c5d0f02
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2024-03-02 12:05:34 +0200

    feat(service-category): Moved package-info.java from ./category.payload to ./payload

--- src/main/java/com/stepaniuk/zrobleno/service/payload/category/package-info.java
+++ src/main/java/com/stepaniuk/zrobleno/service/payload/category/package-info.java
@@ -1,6 +1,6 @@
 @NonNullApi
 @NonNullFields
-package com.stepaniuk.zrobleno.service.category.payload;
+package com.stepaniuk.zrobleno.service.payload.category;
 
 import org.springframework.lang.NonNullApi;
 import org.springframework.lang.NonNullFields;
