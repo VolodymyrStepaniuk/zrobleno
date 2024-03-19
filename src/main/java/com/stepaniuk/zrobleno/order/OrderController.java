3d0dc99fa04c7e6625ef5bed4dc2e3155c65f762
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2024-03-19 18:51:14 +0200

    feat(order): Added some new endpoints

--- src/main/java/com/stepaniuk/zrobleno/order/OrderController.java
+++ src/main/java/com/stepaniuk/zrobleno/order/OrderController.java
@@ -3,9 +3,12 @@ package com.stepaniuk.zrobleno.order;
 import com.stepaniuk.zrobleno.order.payload.OrderCreateRequest;
 import com.stepaniuk.zrobleno.order.payload.OrderResponse;
 import com.stepaniuk.zrobleno.order.payload.OrderUpdateRequest;
+import com.stepaniuk.zrobleno.order.status.OrderStatusName;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -24,7 +27,8 @@ public class OrderController {
 
   @PostMapping
   public ResponseEntity<OrderResponse> createOrder(@RequestBody OrderCreateRequest request) {
-    return ResponseEntity.ok(orderService.createOrder(request));
+    return new ResponseEntity<>(orderService.createOrder(request),
+        HttpStatus.CREATED);
   }
 
   @GetMapping("/{id}")
@@ -38,13 +42,33 @@ public class OrderController {
     return ResponseEntity.ok(orderService.updateOrder(id, request));
   }
 
+  @PostMapping("/{id}/cancel")
+  public ResponseEntity<OrderResponse> cancelOrderById(@PathVariable Long id) {
+    return ResponseEntity.ok(orderService.changeStatus(id, OrderStatusName.CANCELED));
+  }
+
+  @PostMapping("/{id}/confirm")
+  public ResponseEntity<OrderResponse> confirmOrderById(@PathVariable Long id) {
+    return ResponseEntity.ok(orderService.changeStatus(id, OrderStatusName.CONFIRMED));
+  }
+
+  @PostMapping("/{id}/in-progress")
+  public ResponseEntity<OrderResponse> setOrderInProgressById(@PathVariable Long id) {
+    return ResponseEntity.ok(orderService.changeStatus(id, OrderStatusName.IN_PROGRESS));
+  }
+
+  @PostMapping("/{id}/complete")
+  public ResponseEntity<OrderResponse> completeOrderById(@PathVariable Long id) {
+    return ResponseEntity.ok(orderService.changeStatus(id, OrderStatusName.COMPLETED));
+  }
+
   @GetMapping("/v1")
   public ResponseEntity<Page<OrderResponse>> getAllOrders(Pageable pageable) {
     return ResponseEntity.ok(orderService.getAllOrders(pageable, null));
   }
 
   @GetMapping("/v2")
-  public ResponseEntity<Page<OrderResponse>> getAllOrders(Pageable pageable, Long ownerId) {
+  public ResponseEntity<Page<OrderResponse>> getAllOrders(Pageable pageable, UUID ownerId) {
     return ResponseEntity.ok(orderService.getAllOrders(pageable, ownerId));
   }
 }
