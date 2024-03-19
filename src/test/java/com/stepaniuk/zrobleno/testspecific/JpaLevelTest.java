42075ac4863e79f37d145a462cbb8fba9ebe70e9
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2024-03-19 18:57:14 +0200

    test(test-specific): Added JpaLevelTest

---
+++ src/test/java/com/stepaniuk/zrobleno/testspecific/JpaLevelTest.java
@@ -0,0 +1,29 @@
+package com.stepaniuk.zrobleno.testspecific;
+
+import com.stepaniuk.zrobleno.audit.JpaAuditConfig;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@AutoConfigureTestDatabaseContainer
+@ImportAutoConfiguration
+@Import({JpaAuditConfig.class})
+public @interface JpaLevelTest {
+
+}
