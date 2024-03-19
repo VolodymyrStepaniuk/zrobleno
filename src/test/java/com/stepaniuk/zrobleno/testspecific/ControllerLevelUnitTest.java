a934b5097b85155171e6037a14c7f922c3a0acd6
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2024-03-19 18:57:05 +0200

    test(test-specific): Added ControllerLevelUnitTest

---
+++ src/test/java/com/stepaniuk/zrobleno/testspecific/ControllerLevelUnitTest.java
@@ -0,0 +1,31 @@
+package com.stepaniuk.zrobleno.testspecific;
+
+import com.stepaniuk.zrobleno.security.WebSecurityConfig;
+import com.stepaniuk.zrobleno.shared.GeneralControllerExceptionHandler;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(properties = "spring.mvc.problem-details.enabled=true")
+@Import({ GeneralControllerExceptionHandler.class, WebSecurityConfig.class})
+@ImportAutoConfiguration
+public @interface ControllerLevelUnitTest {
+
+  @AliasFor(attribute = "controllers", annotation = WebMvcTest.class) Class<?>[] controllers() default {};
+}
